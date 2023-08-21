d7ffa01c45cd2eb1901dd8063de2810e60fdc562
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-08-21 06:05:37 +0430

    Update views.py

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+
 views = Blueprint('views', __name__)
 
 @views.route('/')
